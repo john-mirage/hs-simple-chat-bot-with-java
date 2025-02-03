da26555d1f53ca6562a36a1d60e8fed275b9bb29
John Mirage <28307090+john-mirage@users.noreply.github.com>
2025-02-03 10:02:07 +0100

    Create UserInterface.java

---
+++ bot/UserInterface.java
@@ -0,0 +1,51 @@
+package bot;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String inputString() {
+        while (true) {
+            String input = scanner.nextLine();
+            if (Validator.stringIsNotEmpty(input)) {
+                return input;
+            } else {
+                System.out.println("Error: the input is empty");
+            }
+        }
+    }
+
+    private static int inputInteger() {
+        while (true) {
+            String input = inputString();
+            try {
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: the input is not a number");
+            }
+        }
+    }
+
+    public static int inputPositiveNumberOrZero() {
+        while (true) {
+            int number = inputInteger();
+            if (Validator.integerIsPositiveOrZero(number)) {
+                return number;
+            } else {
+                System.out.println("Error: the input is not a positive number or zero");
+            }
+        }
+    }
+
+    public static int inputPositiveNumber() {
+        while (true) {
+            int number = inputInteger();
+            if (Validator.integerIsPositive(number)) {
+                return number;
+            } else {
+                System.out.println("Error: the input is not a positive number");
+            }
+        }
+    }
+}
